a58d9f2bf77c47b41467ad062dc41df05a3a29ee
youkwhd <lolywk@tutanota.com>
2022-05-10 18:16:07 +0700

    rename main.py as run.py

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
 from app import create_app
 
 app = create_app()
-print(__name__)
-
-@app.route("/")
-def home():
-    return "welcome to my webpage"
 
 if __name__ == "__main__":
     app.run(debug=True)
