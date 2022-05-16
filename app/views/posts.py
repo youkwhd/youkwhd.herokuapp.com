8bfbc254d7d130af514fc98b3bbe78f03f289207
youkwhd <lolywk@tutanota.com>
2022-05-16 14:26:06 +0700

    feat: nested table of contents, needs refactoring

--- app/views/posts.py
+++ app/views/posts.py
@@ -35,20 +35,75 @@ def post(slug):
             toc_tag = soup.new_tag("h2")
             toc_tag.string = str(c).strip()
 
-            ul_tag = soup.new_tag("ul")
+            # TODO: refactor nested toc, this is yet the worst code i've wrotten, some of it is hard coded
+            heading_tags = soup.find_all(re.compile('^h[1-6]$'))
+            root_ul_tag = soup.new_tag("ul")
+            root_li = soup.new_tag("li")
 
-            for heading_tag in soup.find_all(re.compile('^h[1-6]$')):
-                li_tag = soup.new_tag("li")
+            a_tag = soup.new_tag("a")
+            a_tag.string = heading_tags[0].string
+            a_tag["href"] = "#" + heading_tags[0]["id"]
 
-                a_tag = soup.new_tag("a")
-                a_tag.string = heading_tag.string
-                a_tag["href"] = "#" + heading_tag["id"]
+            root_li_name = int(heading_tags[0].name[1:len(heading_tags[0].name)])
+            root_li.append(a_tag)
+            root_li["class"] = str(root_li_name)
 
-                li_tag.append(a_tag)
-                ul_tag.append(li_tag)
+            root_ul_tag.append(root_li)
+
+            last_li_nested = None
+            for i in range(1, len(heading_tags)):
+                heading_tag_number_now = int(heading_tags[i].name[1:len(heading_tags[i].name)])
+                heading_tag_number_before = int(heading_tags[i - 1].name[1:len(heading_tags[i - 1].name)])
+                is_root = heading_tag_number_now == root_li_name
+
+                if (not is_root):
+                    temp_a_tag = soup.new_tag("a")
+                    temp_a_tag.string = heading_tags[i].string
+                    temp_a_tag["href"] = "#" + heading_tags[i]["id"]
+
+                    temp_li_tag = soup.new_tag("li")
+                    temp_li_tag["class"] = str(heading_tag_number_now)
+                    temp_li_tag.append(temp_a_tag)
+
+                    temp_ul_tag = soup.new_tag("ul")
+                    temp_ul_tag.append(temp_li_tag)
+
+                    if (heading_tag_number_now < heading_tag_number_before):
+                        new_nest_tag = root_ul_tag.find_all(class_=str(heading_tag_number_now - 1))
+                        new_nest_tag[-1].append(temp_ul_tag)
+                        last_li_nested = new_nest_tag[-1]
+
+                    if (last_li_nested != None):
+                        last_li_nested.append(temp_ul_tag)
+                    else:
+                        root_li.append(temp_ul_tag)
+                        
+                    if (i + 1 < len(heading_tags) and heading_tag_number_now < int(heading_tags[i + 1].name[1:len(heading_tags[i + 1].name)])):
+                        last_li_nested = temp_li_tag
+
+                if (is_root):
+                    temp_a_tag = soup.new_tag("a")
+                    temp_a_tag.string = heading_tags[i].string
+                    temp_a_tag["href"] = "#" + heading_tags[i]["id"]
+
+                    temp_li_tag = soup.new_tag("li")
+                    temp_li_tag["class"] = str(heading_tag_number_now)
+                    temp_li_tag.append(temp_a_tag)
+
+                    temp_ul_tag = soup.new_tag("ul")
+                    temp_ul_tag.append(temp_li_tag)
+
+                    root_li.append(temp_li_tag)
+
+                    root_li = temp_li_tag
+                    last_li_nested = None
+
+            # clean up temp classes
+            for tag in root_ul_tag.find_all():
+                del tag["class"]
 
             c.insert_before(toc_tag)
-            c.insert_before(ul_tag)
+            c.insert_before(root_ul_tag)
 
             # delete the comment
             c.extract()
